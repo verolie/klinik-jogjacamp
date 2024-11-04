da06427c918e6a5f27da08f9b85f346b040f1a5a
verolie <veyolie@gmail.com>
2024-11-05 06:58:24 +0700

    fix error token

--- klinik-jogjacamp/app/Models/Patient.php
+++ klinik-jogjacamp/app/Models/Patient.php
@@ -9,4 +9,6 @@ class Patient extends Model
 {
     use HasFactory;
     protected $table = 'patients';
+
+    protected $fillable = ['name'];
 }
--- klinik-jogjacamp/routes/web.php
+++ klinik-jogjacamp/routes/web.php
@@ -6,12 +6,16 @@ use App\Http\Controllers\DiagnoseController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\AppointmentController;
 
-Route::post('/api/patient', [PatientController::class, 'createPatient']);
-Route::post('/api/diagnose', [DiagnoseController::class, 'createDiagnose']);
-Route::post('/api/service', [ServiceController::class, 'createService']);
+Route::get('/token', function () {
+    return csrf_token();
+});
 
+Route::middleware('api')->prefix('api')->group(function () {
+    Route::post('/patient', [PatientController::class, 'createPatient']);
+    Route::post('/diagnose', [DiagnoseController::class, 'createDiagnose']);
+    Route::post('/service', [ServiceController::class, 'createService']);
 
-Route::post('/api/appointment', [AppointmentController::class, 'createAppointment']);
-Route::get('/api/appointment/{id}', [AppointmentController::class, 'getAppointment']);
-
-Route::patch('/api/appointment/{id}', [AppointmentController::class, 'patchAppointment']);
+    Route::post('/appointment', [AppointmentController::class, 'createAppointment']);
+    Route::get('/appointment/{id}', [AppointmentController::class, 'getAppointment']);
+    Route::patch('/appointment/{id}', [AppointmentController::class, 'patchAppointment']);
+});
